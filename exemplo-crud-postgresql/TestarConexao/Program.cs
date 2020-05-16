ee2d15af3b7b7dad8889c5d83d5e09a27aca04f6
marcelo.abezerra@gmail.com <bezerraMarcelo>
2020-05-16 18:44:00 -0300

    Commit inicial - estrutura do projeto

---
+++ exemplo-crud-postgresql/TestarConexao/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestarConexao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
