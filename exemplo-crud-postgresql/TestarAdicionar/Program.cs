408711f04dd50092356c3ccf72fe83f85b1ce181
marcelo.abezerra@gmail.com <bezerraMarcelo>
2020-05-19 22:14:30 -0300

    Testar Adicionar

---
+++ exemplo-crud-postgresql/TestarAdicionar/Program.cs
@@ -0,0 +1,24 @@
+﻿using biblioteca.modelo;
+using biblioteca.dao;
+using System;
+
+namespace TestarAdicionar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Contato contato = new Contato();
+
+            contato.Nome = "Rafael";
+            contato.Email = "rafael@teste.com.br";
+            contato.DataNascimento = Convert.ToDateTime("2017-05-30");
+
+            ContatoDao dao = new ContatoDao();
+
+            dao.Adicionar(contato);
+
+            Console.Out.WriteLine("Gravado!");
+        }
+    }
+}
--- exemplo-crud-postgresql/biblioteca/dao/ContatoDao.cs
+++ exemplo-crud-postgresql/biblioteca/dao/ContatoDao.cs
@@ -1,10 +1,39 @@
-﻿using System;
+﻿using biblioteca.modelo;
+using Npgsql;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace biblioteca.dao
 {
-    class ContatoDao
+    public class ContatoDao
     {
+        private NpgsqlConnection conexao;
+
+        public ContatoDao()
+        {
+            this.conexao = new FabricaConexoes().BuscarConexao();
+        }
+
+        public void Adicionar(Contato contato)
+        {
+            conexao.Open();
+            try
+            {
+                string comando = "insert into contatos (nome, email, data_nascimento) values (@p1, @p2, @p3)";
+
+                using (var cmd = new NpgsqlCommand(comando, conexao))
+                {
+                    cmd.Parameters.AddWithValue("p1", contato.Nome);
+                    cmd.Parameters.AddWithValue("p2", contato.Email);
+                    cmd.Parameters.AddWithValue("p3", contato.DataNascimento);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }
     }
 }
